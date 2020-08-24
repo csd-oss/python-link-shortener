37655cb1acbcdcb7634c5dcbbcd55c8337150e2f
csd-oss <mk@buialo.com>
2020-08-24 20:55:45 +0300

    created url class

---
+++ url.py
@@ -0,0 +1,5 @@
+class Url:
+    def __init__(self, shortId="", redirectTo="", callbackUrl=""):
+        self.__shortId = shortId
+        self.__redirectTo = redirectTo
+        self.__callbackUrl = callbackUrl

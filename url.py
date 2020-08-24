7d94dea2fa780acaee3dc2f6205b537df4880391
csd-oss <mk@buialo.com>
2020-08-24 21:45:46 +0300

    parse_device function added

--- url.py
+++ url.py
@@ -1,5 +1,33 @@
+from device_detector import DeviceDetector
+
+
 class Url:
-    def __init__(self, shortLink="", redirectTo="", callbackUrl=""):
+    def __init__(
+                self, shortLink="", redirectTo="",
+                callbackUrl="", _id="", userAgent=""):
         self.__shortLink = shortLink
         self.__redirectTo = redirectTo
         self.__callbackUrl = callbackUrl
+        self._id = _id
+        self.__userAgent = userAgent
+
+    def parse_device(self):
+        device = DeviceDetector(self.__userAgent).parse()
+        deviceObj = {
+            'isBot': device.is_bot(),
+            'osName': device.os_name(),
+            'osVersion': device.os_version(),
+            'brand': device.device_brand_name(),
+            'model': device.device_model(),
+            'type': device.device_type(),
+            'clientName': device.client_name(),
+            'clientType': device.client_type(),
+            'clientVersion': device.client_version(),
+            'engine': device.engine()
+        }
+        self.__device = deviceObj
+
+
+url = Url()
+
+print(url.__dict__)

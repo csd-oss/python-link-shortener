1f70de800676ae5f1aea407c0ba3aabe6f0a69fd
csd-oss <mk@buialo.com>
2020-08-24 14:30:51 +0300

    closses #4

--- device_parser.py
+++ device_parser.py
@@ -10,6 +10,10 @@ def device_parser(ua):
         'osVersion': device.os_version(),
         'brand': device.device_brand_name(),
         'model': device.device_model(),
-        'type': device.device_type()
+        'type': device.device_type(),
+        'clientName': device.client_name(),
+        'clientType': device.client_type(),
+        'clientVersion': device.client_version(),
+        'engine': device.engine()
     }
     return deviceObj

fc030b548ee67c878797f5bf6df0d06b855c76a8
csd-oss <mk@buialo.com>
2020-08-24 22:26:27 +0300

    added sent callback function

--- url.py
+++ url.py
@@ -1,33 +1,58 @@
+import json
+import time
+
+import requests
 from device_detector import DeviceDetector
 
 
 class Url:
     def __init__(
                 self, shortLink="", redirectTo="",
-                callbackUrl="", _id="", userAgent=""):
+                callbackUrl="", _id="", userAgent=""
+                ):
         self.__shortLink = shortLink
         self.__redirectTo = redirectTo
         self.__callbackUrl = callbackUrl
         self._id = _id
         self.__userAgent = userAgent
 
-    def parse_device(self):
+    def send_callbak(self):
+        if self.__callbackUrl != "":
+            headers = {'content-type': 'application/json'}
+            data = self.__url_obj()
+            data['timestamp'] = time.time()
+            print(json.dumps(data))
+            requests.request(
+                "POST", self.__callbackUrl, data=json.dumps(data),
+                headers=headers
+            )
+        else:
+            pass
+
+    def __parse_device(self):
         device = DeviceDetector(self.__userAgent).parse()
         deviceObj = {
-            'isBot': device.is_bot(),
-            'osName': device.os_name(),
-            'osVersion': device.os_version(),
-            'brand': device.device_brand_name(),
-            'model': device.device_model(),
-            'type': device.device_type(),
-            'clientName': device.client_name(),
-            'clientType': device.client_type(),
-            'clientVersion': device.client_version(),
-            'engine': device.engine()
+            "isBot": device.is_bot(),
+            "osName": device.os_name(),
+            "osVersion": device.os_version(),
+            "brand": device.device_brand_name(),
+            "model": device.device_model(),
+            "type": device.device_type(),
+            "clientName": device.client_name(),
+            "clientType": device.client_type(),
+            "clientVersion": device.client_version(),
+            "engine": device.engine()
         }
         self.__device = deviceObj
 
-
-url = Url()
-
-print(url.__dict__)
+    def __url_obj(self):
+        self.__parse_device()
+        url_obj = {
+            "device": self.__device,
+            "shortLink": self.__shortLink,
+            "redirectTo": self.__redirectTo,
+            "callbackUrl": self.__callbackUrl,
+            "_id": self._id,
+            "userAgent": self.__userAgent
+            }
+        return url_obj
